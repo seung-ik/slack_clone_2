22c51dd0c56d388cdc14a4f630f9c40fc42fa22c
seung-ik <69374857+seung-ik@users.noreply.github.com>
2021-03-31 21:14:08 +0900

    signup component jsx 2/5

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,12 +1,43 @@
-import React from 'react'
-import { Header } from '@pages/Signup/style'
+import React, { ChangeEvent, useState } from 'react'
+import { Header ,Label,Input,Container,Form,Button} from '@pages/Signup/style'
+import { Link } from 'react-router-dom'
 
 
 const Signup = () => {
+  const [email,setEmail]=useState("")
+  const [nickname,setnickname]=useState("")
+  const [password,setpassword]=useState("")
+  const [passwordCheck,setpasswordCheck]=useState("")
+  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>)=>{
+    setEmail(e.target.value)
+  }
   return (
-    <Header>
-      회원가입페이지
-    </Header>
+    <Container>
+      <Form>
+        <Header>회원가입</Header>
+        <Label>
+          <span>email</span>
+          <Input type="text" id="email" name="email" value={email} onChange={onChangeEmail}></Input>
+        </Label>
+        <Label>
+          <span>닉네임</span>
+          <Input type="text" id="nickname" name="nickname" value={nickname} onChange={onChangeEmail}></Input>
+        </Label>
+        <Label>
+          <span>패스워드</span>
+          <Input type="password" id="password" name="password" value={password} onChange={onChangeEmail}></Input>
+        </Label>
+        <Label>
+          <span>패스워드 확인</span>
+          <Input type="password" id="passwordCheck" name="passwordCheck" value={passwordCheck} onChange={onChangeEmail}></Input>
+        </Label>
+        <Button>회원가입완료</Button>
+      </Form>
+      <div>
+        이미 회원이신가요?&nbsp;&nbsp;&nbsp;
+        <Link to="/login">로그인 하러가기</Link>
+      </div>
+    </Container>
   )
 }
 
--- src/pages/Signup/style.tsx
+++ src/pages/Signup/style.tsx
@@ -2,4 +2,34 @@ import styled from '@emotion/styled'
 
 export const Header = styled.header`
   font-size: 48px;
+  margin : 30px;
+  text-align: center;
+`;
+
+export const Label = styled.label`
+  font-size: 30px;
+  display:flex;
+  flex-direction:column;
+  margin-bottom:10px;
+`;
+export const Input = styled.input`
+  font-size: 48px;
+`;
+
+export const Container = styled.div`
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+`;
+export const Form = styled.div`
+  display:flex;
+  flex-direction:column;
+  padding:10px;
+`;
+
+export const Button = styled.button`
+  padding: 20px;
+  margin-top: 20px;
+  font-size:30px;
 `;
